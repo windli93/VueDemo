06fd203db546c97ba991f8f24a527ebbd649118a
v_ragrokli <v_ragrokli@lexinfintech.com>
2019-02-13 12:57:31 +0800

    优化项目

---
+++ Spring-Vue.impl/src/main/java/Main.java
@@ -0,0 +1,14 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description:
+ * @author: ragrokli
+ * @create: 2019-02-13 12:54
+ **/
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
