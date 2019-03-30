7425bc8c209687dd0ed7a4a979a38b3497b0b4a8
ragrok <ragrokli@hotmail.com>
2019-03-30 20:32:17 +0800

    学习vue基础知识

---
+++ vue_test1/src/todoList.vue
@@ -0,0 +1,39 @@
+<template>
+<div>
+   <input v-model="inputValue"/>
+   <button @click="hadleSubmit">提交按钮</button>
+   <div>
+   <ul>
+      <todo-item v-for="(item, index) in list" :key="index" :item="item" :index="index" @delete-on="hadleDelete"></todo-item>
+   </ul>
+   </div>
+</div>
+</template>
+
+<script>
+   import todoItem from "./components/item.vue";
+   export default{
+     components: {
+          "todo-item": todoItem,
+     },
+     data(){
+       return{
+         inputValue: "",
+         list: [],
+       }
+     },
+     methods: {
+        hadleSubmit(){
+            this.list.push(this.inputValue);
+            this.inputValue = "";
+        },
+        hadleDelete(index){
+            this.list.splice(index,1);
+        }
+     },
+   }
+</script>
+
+<style>
+
+</style>
