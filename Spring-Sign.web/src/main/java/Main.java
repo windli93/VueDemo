3c4513a44f858af09bbee9cd889bd013377ac518
ragrokli <ragrokli@hotmail.com>
2019-04-01 15:15:33 +0800

    添加技术方案文档

--- Spring-Sign.web/src/main/java/Main.java
+++ Spring-Sign.web/src/main/java/Main.java
@@ -2,7 +2,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @ComponentScan("cn.com.github")
 @EnableAutoConfiguration
